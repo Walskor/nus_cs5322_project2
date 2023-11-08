6ef30e7f1f7df7fb7aa4e9c1829a290bda8cad32
walskor <1148905353@qq.com>
2023-11-08 16:18:50 +0800

    add test sql

--- Code/test.sql
+++ Code/test.sql
@@ -19,3 +19,14 @@ select * from INSURANCE.USERS; --can see id 4
 SELECT SA_SESSION.LABEL('PROJECT_OLS_POL') FROM DUAL; --label=TS:C:EA,WA
 
 select * from INSURANCE.USERS; --can see id 5
+
+
+
+
+-- get row label info
+SELECT LABEL_TO_CHAR(OLS_COL) AS "Label",
+       SUBSTR(LABEL_TO_CHAR(OLS_COL), 1, INSTR(LABEL_TO_CHAR(OLS_COL), ':', 1) - 1) AS "FirstPart",
+       SUBSTR(LABEL_TO_CHAR(OLS_COL), INSTR(LABEL_TO_CHAR(OLS_COL), ':', 1) + 1,
+              LENGTH(LABEL_TO_CHAR(OLS_COL)) - 2 * INSTR(LABEL_TO_CHAR(OLS_COL), ':') - 1) AS "MiddlePart",
+       SUBSTR(LABEL_TO_CHAR(OLS_COL), INSTR(LABEL_TO_CHAR(OLS_COL), ':', -1) + 1) AS "LastPart"
+FROM INSURANCE.USERS;
